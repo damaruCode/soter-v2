2fe35ada64b2c9edae266f5f02884714332048ef
damaruCode <david.rumscheid@googlemail.com>
2025-02-24 16:08:00 +0100

    Output the form list

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,18 @@
 use std::fmt;
+use std::panic;
+use std::sync::Arc;
 
 use elp_base_db::salsa;
 use elp_base_db::FileId;
 use elp_base_db::FileLoaderDelegate;
-use elp_base_db::SourceDatabaseExt;
-use std::panic;
-use std::sync::Arc;
+use elp_base_db::FileRange;
+use elp_base_db::SourceDatabase;
+use elp_base_db::Upcast;
+use elp_types_db::eqwalizer;
+use elp_types_db::IncludeGenerated;
+use elp_types_db::TypedSemantic;
+
+use hir::db::InternDatabase;
 
 #[salsa::database(
     elp_base_db::SourceDatabaseExtStorage,
@@ -18,14 +25,19 @@ pub(crate) struct Db {
     storage: salsa::Storage<Self>,
 }
 
-impl Db {
-    pub fn set_input(&mut self, input: String) {
-        let f_id = FileId::from_raw(0);
-        self.set_file_text(f_id, input.into());
+impl salsa::Database for Db {}
+
+impl Upcast<dyn SourceDatabase> for Db {
+    fn upcast(&self) -> &(dyn SourceDatabase + 'static) {
+        self
     }
 }
 
-impl salsa::Database for Db {}
+impl Upcast<dyn InternDatabase> for Db {
+    fn upcast(&self) -> &(dyn InternDatabase + 'static) {
+        self
+    }
+}
 
 impl fmt::Debug for Db {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -38,5 +50,22 @@ impl panic::RefUnwindSafe for Db {}
 impl elp_base_db::FileLoader for Db {
     fn file_text(&self, file_id: elp_base_db::FileId) -> Arc<str> {
         FileLoaderDelegate(self).file_text(file_id)
+    }
+}
+
+impl TypedSemantic for Db {
+    fn eqwalizer_diagnostics(
+        &self,
+        _file_id: FileId,
+        _include_generated: IncludeGenerated,
+    ) -> Option<Vec<eqwalizer::EqwalizerDiagnostic>> {
+        panic!("Eqwalizer data is not available in HIR tests")
+    }
+
+    fn eqwalizer_type_at_position(
+        &self,
+        _range: elp_base_db::FileRange,
+    ) -> Option<Arc<(eqwalizer::types::Type, FileRange)>> {
+        panic!("Eqwalizer data is not available in HIR tests")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 pub mod db;
+use elp_base_db::FileId;
+use elp_base_db::SourceDatabaseExt;
 
 fn main() {
     let file = std::fs::read_to_string("erlang-code/fib.erl").expect("File should be readable");
-
-    // erl_syntax
-    // let parse = elp_syntax::SourceFile::parse_text(&file);
-    // let actual_tree = format!("{:#?}", parse.syntax_node());
-    // println!("{}", actual_tree);
-
     let mut db = db::Db::default();
-    db.set_input(file);
-    hir::Semantic::new(&db);
+
+    let f_id = FileId::from_raw(0);
+    db.set_file_text(f_id, file.into());
+
+    let sema = hir::Semantic::new(&db);
+    let form_list = sema.form_list(f_id);
+    dbg!(form_list);
 }
