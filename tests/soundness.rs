1bbe13d9e6aa6974664ed1931fc51e906798b8e6
damaruCode <david.rumscheid@googlemail.com>
2025-11-17 17:23:16 +0100

    Other tests

--- tests/soundness.rs
+++ tests/soundness.rs
@@ -5,6 +5,7 @@ use soter_v2::analyzer::Analyzer;
 use soter_v2::ast;
 use soter_v2::ast::TypedCore;
 use soter_v2::erlang;
+use soter_v2::state_space::Mailboxes;
 use soter_v2::state_space::Store;
 use soter_v2::state_space::Value;
 use soter_v2::state_space::VarName;
@@ -34,11 +35,32 @@ fn check_store(
                             _ => panic!("{} is not a literal", tc),
                         }
                     }
-                    _ => panic!("{} is not a closure", value),
+                    _ => panic!("{} is not a closure", value), // TODO in concurr this fails
                 }
             }
         }
     }
+}
+
+fn check_mailboxes(mailboxes: Mailboxes<VAddr>) {
+    panic!("{}", &format!("{:?}", mailboxes)); // TODO impl
+}
+
+#[test]
+fn test_standard_concurr() {
+    erlang::run(&format!("tests/icfa_examples/concurr.erl"));
+    let core = erlang::get_core(&format!("tests/icfa_examples/concurr.erl.json"));
+    let typed_core = ast::TypedCore::from(core);
+    let mut ast_helper = AstHelper::new();
+    let indexed_typed_core = ast_helper.build_indecies(typed_core);
+    ast_helper.build_lookup(&indexed_typed_core);
+    let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
+
+    let (_ps, m, s) = analyzer.run();
+
+    check_store(&s, ast_helper.clone(), "P", vec![]);
+
+    check_mailboxes(m);
 }
 
 #[test]
@@ -51,9 +73,30 @@ fn test_standard_id() {
     ast_helper.build_lookup(&indexed_typed_core);
     let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
 
-    let (_ps, _m, s) = analyzer.run();
+    let (_ps, m, s) = analyzer.run();
 
     check_store(&s, ast_helper.clone(), "Y", vec!["a"]);
     check_store(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
     check_store(&s, ast_helper.clone(), "X", vec!["a", "b"]);
+
+    check_mailboxes(m);
+}
+
+#[test]
+fn test_standard_rec_id() {
+    erlang::run(&format!("tests/icfa_examples/rec_id.erl"));
+    let core = erlang::get_core(&format!("tests/icfa_examples/rec_id.erl.json"));
+    let typed_core = ast::TypedCore::from(core);
+    let mut ast_helper = AstHelper::new();
+    let indexed_typed_core = ast_helper.build_indecies(typed_core);
+    ast_helper.build_lookup(&indexed_typed_core);
+    let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
+
+    let (_ps, m, s) = analyzer.run();
+
+    check_store(&s, ast_helper.clone(), "Y", vec!["a"]);
+    check_store(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
+    check_store(&s, ast_helper.clone(), "X", vec!["a", "b"]);
+
+    check_mailboxes(m);
 }
