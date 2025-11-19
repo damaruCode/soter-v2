33273baa376640282c46b8ba260e489f7c0a5997
damaruCode <david.rumscheid@googlemail.com>
2025-11-19 16:22:54 +0100

    More tests

--- tests/soundness.rs
+++ tests/soundness.rs
@@ -6,12 +6,15 @@ use soter_v2::ast;
 use soter_v2::ast::TypedCore;
 use soter_v2::erlang;
 use soter_v2::state_space::Mailboxes;
+use soter_v2::state_space::Pid;
+use soter_v2::state_space::ProcState;
 use soter_v2::state_space::Store;
 use soter_v2::state_space::Value;
 use soter_v2::state_space::VarName;
 use soter_v2::util::AstHelper;
+use soter_v2::util::SetMap;
 
-fn check_store(
+fn check_closure(
     store: &Store<KAddr, VAddr>,
     ast_helper: AstHelper,
     var_name: &str,
@@ -35,7 +38,44 @@ fn check_store(
                             _ => panic!("{} is not a literal", tc),
                         }
                     }
-                    _ => panic!("{} is not a closure", value), // TODO in concurr this fails
+                    _ => panic!("{} is not a closure", value),
+                }
+            }
+        }
+    }
+}
+
+fn check_process(
+    proc_states: &SetMap<Pid, ProcState<KAddr, VAddr>>,
+    store: &Store<KAddr, VAddr>,
+    ast_helper: AstHelper,
+    var_name: &str,
+) {
+    for (vaddr, val) in &store.value.inner {
+        if vaddr.var_name == VarName::Atom(var_name.to_string()) {
+            for value in val {
+                match value {
+                    Value::Pid(p) => {
+                        proc_states
+                            .get(p)
+                            .expect(format!("{} is not pointing to a proc_state", p).as_str());
+                        let tc = ast_helper.get(p.prog_loc);
+                        match tc {
+                            TypedCore::Call(c) => match *c.name.clone() {
+                                TypedCore::Literal(l) => match *l.val.clone() {
+                                    TypedCore::String(erls) => {
+                                        if !erls.inner.eq("spawn") {
+                                            panic!("\"{}\" is not a spawn", erls);
+                                        }
+                                    }
+                                    _ => panic!("{} is not a string", l),
+                                },
+                                _ => panic!("{} is not a literal", c),
+                            },
+                            _ => panic!("{} is not a call", tc),
+                        }
+                    }
+                    _ => panic!("{} is not a pid", value),
                 }
             }
         }
@@ -56,11 +96,12 @@ fn test_standard_concurr() {
     ast_helper.build_lookup(&indexed_typed_core);
     let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
 
-    let (_ps, m, s) = analyzer.run();
+    let (ps, _m, s) = analyzer.run();
 
-    check_store(&s, ast_helper.clone(), "P", vec![]);
+    check_process(&ps, &s, ast_helper.clone(), "P");
+    check_process(&ps, &s, ast_helper.clone(), "X");
 
-    check_mailboxes(m);
+    //check_mailboxes(m);
 }
 
 #[test]
@@ -73,13 +114,13 @@ fn test_standard_id() {
     ast_helper.build_lookup(&indexed_typed_core);
     let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
 
-    let (_ps, m, s) = analyzer.run();
+    let (_ps, _m, s) = analyzer.run();
 
-    check_store(&s, ast_helper.clone(), "Y", vec!["a"]);
-    check_store(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
-    check_store(&s, ast_helper.clone(), "X", vec!["a", "b"]);
+    check_closure(&s, ast_helper.clone(), "Y", vec!["a"]);
+    check_closure(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
+    check_closure(&s, ast_helper.clone(), "X", vec!["a", "b"]);
 
-    check_mailboxes(m);
+    //check_mailboxes(m);
 }
 
 #[test]
@@ -92,11 +133,11 @@ fn test_standard_rec_id() {
     ast_helper.build_lookup(&indexed_typed_core);
     let mut analyzer = Analyzer::new(ast_helper.clone(), Box::new(StandardAbstraction::new(0)));
 
-    let (_ps, m, s) = analyzer.run();
+    let (_ps, _m, s) = analyzer.run();
 
-    check_store(&s, ast_helper.clone(), "Y", vec!["a"]);
-    check_store(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
-    check_store(&s, ast_helper.clone(), "X", vec!["a", "b"]);
+    check_closure(&s, ast_helper.clone(), "Y", vec!["a"]);
+    check_closure(&s, ast_helper.clone(), "Z", vec!["a", "b"]);
+    check_closure(&s, ast_helper.clone(), "X", vec!["a", "b"]);
 
-    check_mailboxes(m);
+    //check_mailboxes(m);
 }
