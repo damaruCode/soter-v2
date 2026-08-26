a1cfa88cda118dbd9c0a2c418f3d2ce42df42432
damaruCode <david.rumscheid@googlemail.com>
2026-08-26 09:53:39 +0200

    example in lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ pub mod ast;
 pub mod erlang;
 pub mod state_space;
 pub mod util;
+
+pub fn test_ast_builder() {
+    let mut clause = ast::Clause::new();
+    clause.body = Box::new(ast::TypedCore::Bool(ast::ErlBool::new(false)));
+}
